a02f91d45b25b20efc7dad82a24f6499724c75cc
Fado <fadocastillo@gmail.com>
2023-06-08 16:39:32 -0300

    Agregue una e para probar

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', function() {
+  const buttonMenuToggle = document.querySelector('.button-menu-toggle');
+  const isHomePage = window.location.pathname === '/';
+
+  function showAlert() {
+    alert('¡Hola, esta es una alerta desde javascript!');
+  }
+
+  if (isHomePage) {
+    buttonMenuToggle.classList.remove('hide');
+  } else {
+    buttonMenuToggle.classList.add('hide');
+  }
+
+  document.querySelector(".button-menu-toggle").addEventListener("click", function () {
+    document.querySelector(".nav-links").classList.toggle("nav-links-responsive");
+  });
+
+  document.querySelectorAll(".nav-links a").forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      var href = link.getAttribute("href");
+      if (href) {
+        window.location.href = href;
+      }
+    });
+  });
+});
